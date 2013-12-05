299f7f28e5093f1ede447b17b3dea435634a48d8
Ricardo Silva <ricardo@salamandra.cc>
2013-12-05 18:07:05 -0200

    Atualizada versao do pacote

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pagseguro-python',
-    version='0.21',
+    version='0.22',
     description='PagSeguro API v.2 client library ',
     author='Ricardo Silva',
     author_email='rsas79@gmail.com',
